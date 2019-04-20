852234bde651b81ada18b74b9994d0620973d511
abdulsalam <abdulsalam121196@gmail.com>
2019-04-20 15:46:36 +0700

    change bot.js

--- index.js
+++ index.js
@@ -40,10 +40,18 @@ adapter.onTurnError = async (context, error) => {
     console.error(`\n [onTurnError]: ${ error }`);
     // Send a message to the user
     await context.sendActivity(`Oops. Something went wrong!`);
+    // Clear out state
+    await conversationState.clear(context);
+    // Save state changes.
+    await conversationState.saveChanges(context);
 };
 
+let conversationState;
+const memoryStorage = new MemoryStorage();
+conversationState = new ConversationState(memoryStorage);
+
 // Create the main dialog.
-const myBot = new MyBot();
+const myBot = new MyBot(conversationState);
 
 // Listen for incoming requests.
 server.post('/api/messages', (req, res) => {
