08c4bb7d0369e22b25a7b0af4e629e38fe3bc544
abdulsalam <abdulsalam121196@gmail.com>
2019-04-20 15:41:16 +0700

    change bot.js

--- bot.js
+++ bot.js
@@ -3,12 +3,32 @@
 
 const { ActivityHandler } = require('botbuilder');
 
+// Turn counter property
+const TURN_COUNTER_PROPERTY = 'turnCounterProperty';
+
 class MyBot extends ActivityHandler {
-    constructor() {
+    constructor(conversationState) {
         super();
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
-        this.onMessage(async turnContext => { console.log('this gets called'); await turnContext.sendActivity(`Wow '${ turnContext.activity.text }'`); });
-        this.onConversationUpdate(async turnContext => { console.log('this gets called (conversatgion update'); await turnContext.sendActivity('[conversationUpdate event detected]'); });
+        this.countProperty = conversationState.createProperty(TURN_COUNTER_PROPERTY);
+		this.conversationState = conversationState;
+        // this.onMessage(async turnContext => { console.log('this gets called'); await turnContext.sendActivity(`Wow '${ turnContext.activity.text }'`); });
+        // this.onConversationUpdate(async turnContext => { console.log('this gets called (conversatgion update'); await turnContext.sendActivity('[conversationUpdate event detected]'); });
+    }
+
+    async onTurn(turnContext) {
+    	if(turnContext.activity.type === ActivityHandler.Message) {
+			let count = await this.countProperty.get(turnContext);
+            count = count === undefined ? 1 : ++count;
+
+            await turnContext.sendActivity(`${count}: You said "${turnContext.activity.text}" `);
+            await this.countProperty.set(turnContext, count);
+
+    	} else {
+			await turnContext.sendActivity(`[${ turnContext.activity.type } event detected]`);
+    	}
+
+		await this.conversationState.saveChanges(turnContext);
     }
 }
 
